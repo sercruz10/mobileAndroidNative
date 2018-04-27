f703e383e79b153e85efd172ee68ea32f9641a83
Sérgio Cruz <sercruz@sapo.pt>
2018-04-27 11:30:09 +0100

    eventadd27

--- app/src/main/java/sercruz/teste_2018/CalendarActivity.java
+++ app/src/main/java/sercruz/teste_2018/CalendarActivity.java
@@ -1,28 +1,69 @@
 package sercruz.teste_2018;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
     private static final String TAB = "CalendarActivity";
-    private CalendarView mCalendar;
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        mCalendar = (CalendarView)findViewById(R.id.calendarView);
-        mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendar = (CompactCalendarView)findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //add event
+
+        Event ev1 = new Event(Color.RED,1524823598000L,"Teste event day");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = year +"/"+month+"/"+dayOfMonth;
-                Log.d(TAB,"onSelectChanged: date"+date);
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+
+                String data = dateClicked.toString();
+                Log.d(TAB,"DATA: "+data);
+
+                if(dateClicked.toString().compareTo("Fri Apr 27 00:00:00 GMT+01:00 2018") ==0){
+                    Toast.makeText(context,"Teste event day", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(context,"No Event",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormat.format(firstDayOfNewMonth));
 
             }
         });
+
+
     }
 }
